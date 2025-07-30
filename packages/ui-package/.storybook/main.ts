a09d68c354be529c112ea46c7de220bf389fe57a
kazeusagi <toshiki1098@gmail.com>
2025-07-30 12:56:29 +0000

    feat: アーキテクチャの修正

--- packages/ui-package/.storybook/main.ts
+++ packages/ui-package/.storybook/main.ts
@@ -10,7 +10,11 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')));
 }
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    './Configure.mdx',
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+  ],
   addons: [
     // getAbsolutePath('@chromatic-com/storybook'),
     getAbsolutePath('@storybook/addon-docs'),
